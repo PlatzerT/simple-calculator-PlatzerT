e5c27f2677c483af732f934d872ef9151ca47573
Thomas Platzer <thomas.platzer@htl-wels.at>
2020-10-16 13:54:46 +0200

    JavaFX Implementation Calculator GUI erstellt, Buttons and Label hinzugefügt

--- src/Calculator.java
+++ src/Calculator.java
@@ -8,9 +8,9 @@ public class Calculator extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../view/sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("/view/CalculatorView.fxml"));
+        primaryStage.setTitle("Calculator");
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
